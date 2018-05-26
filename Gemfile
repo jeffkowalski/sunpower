7a2893b3bf2be9c6fec33a2b8c29139cb7244c5c
Jeff Kowalski <jeff.kowalski@gmail.com>
2018-05-26 12:44:33 -0700

    Use RestClient rather than Net::HTTP

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
+
 gem 'json'
+gem 'rest-client'
 
 # for robe:
 gem 'pry'
--- sunpower.rb
+++ sunpower.rb
@@ -1,34 +1,29 @@
 #!/usr/bin/env ruby
 
-require 'net/http'
-require 'uri'
-require 'open-uri'
 require 'json'
 require 'yaml/store'
+require 'rest-client'
 
 
 sunpower_credentials = YAML.load_file File.join(Dir.home, '.credentials', "sunpower.yaml")
-api_base_url = "https://monitor.us.sunpower.com/CustomerPortal/"
+api_base_url = "https://monitor.us.sunpower.com/CustomerPortal"
 
-uri = URI.parse(api_base_url + "Auth/Auth.svc/Authenticate")
-http = Net::HTTP.new uri.hostname, uri.port
-http.use_ssl = true
-auth_response = http.send_request 'POST', uri.path, sunpower_credentials.to_json, {'Content-Type' => 'application/json'}
-puts auth_response.body
-decoded = JSON.parse auth_response.body
+response = RestClient.post "#{api_base_url}/Auth/Auth.svc/Authenticate", sunpower_credentials.to_json, {'Content-Type' => 'application/json'}
+authorization = JSON.parse response
+tokenid = authorization['Payload']['TokenID']
 
-tokenid = decoded['Payload']['TokenID']
+response = RestClient.get "#{api_base_url}/CurrentPower/CurrentPower.svc/GetCurrentPower?id=#{tokenid}"
+puts response
+power = JSON.parse response
+puts "#{power['Payload']['CurrentProduction']}kW at #{power['Payload']['SystemList'][0]['DateTimeReceived']}"
 
-current_power_response = open(api_base_url + "CurrentPower/CurrentPower.svc/GetCurrentPower?id=#{tokenid}").read
-puts current_power_response
-decoded = JSON.parse current_power_response
-puts "#{decoded['Payload']['CurrentProduction']}kW at #{decoded['Payload']['SystemList'][0]['DateTimeReceived']}"
 
 class String
   def numeric?
     Float(self) != nil rescue false
   end
 end
+
 
 # similar to https://monitor.us.sunpower.com/v08042016054226/C:/Program Files (x86)/Jenkins/workspace/SunpowerSpa-Development/src/scripts/modules/lifetimeEnergy/lifetimeEnergyService.js#574
 def csvToHashtable csvData
@@ -58,7 +53,8 @@ def csvToHashtable csvData
   return obj
 end
 
+
 TIMESTAMP = '2000-01-01T00:00:00' # long ago
-hourly_energy_data = open(api_base_url + "SystemInfo/SystemInfo.svc/getHourlyEnergyData?tokenid=#{tokenid}&timestamp=#{TIMESTAMP}").read
+hourly_energy_data = RestClient.get "#{api_base_url}/SystemInfo/SystemInfo.svc/getHourlyEnergyData?tokenid=#{tokenid}&timestamp=#{TIMESTAMP}"
 energy_data = csvToHashtable hourly_energy_data
 puts "Lifetime energy = #{energy_data.map{ |_date, values| values[:ep] }.reduce(0, :+)} kWh"
